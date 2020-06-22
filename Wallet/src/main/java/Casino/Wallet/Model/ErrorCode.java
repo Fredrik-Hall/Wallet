1571ddee533bfff6261f06c7383f247ecf6616c0
Fredrik Hall <fh222fk@student.lnu.se>
2020-06-23 00:43:34 +0200

    B2-ControllerAndSwagger
    * Added WalletController with endpoints
    * Added Swagger, Reachable on http://localhost:8080/swagger-ui.html

---
+++ Wallet/src/main/java/Casino/Wallet/Model/ErrorCode.java
@@ -0,0 +1,21 @@
+package Casino.Wallet.Model;
+
+public enum ErrorCode {
+    OK(0),
+    NOT_ENOUGH_FUND (1),
+    TRANSACTION_ID_TAKEN (2),
+    INVALID_AMOUNT (3),
+    INVALID_USER (4),
+    INVALID_TRANSACTION_ID (5);
+
+
+    public final int index;
+
+     ErrorCode(int index) {
+        this.index = index;
+    }
+
+}
+
+
+
